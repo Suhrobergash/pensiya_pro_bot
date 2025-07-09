db966df2dbbd8c08b1c8ae927f487012159c0bc6
Suhrobergash <ergashevsuhrob1999@gmail.com>
2025-07-09 13:46:55 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# 🤖 Bot tokeni
+TOKEN = "7721489051:AAH-C2vgbZBHhaqlkn7dHp_iCtlfLfhmnB0"
+
+# 📣 Tekshiriladigan kanallar ro'yxati
+CHANNELS = [
+    "@pensiya_uzgarishlar",
+    "@uz_Samarqandi",
+    "@RSiiCMSwswIzZmE6",
+    "@music_mrxclay",
+    "@uz_tr_mrx"
+]
